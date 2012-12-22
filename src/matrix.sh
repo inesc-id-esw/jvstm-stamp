3ad927706ccb70ab492c72bbe971c3a948d3f8af
nmldiegues <nmld@ist.utl.pt>
2012-12-22 18:42:52 +0000

    * Branch used for on-going development.

--- src/matrix.sh
+++ src/matrix.sh
@@ -13,6 +13,6 @@ java $1 $2 -cp jvstm.jar:. jstamp.jvstm.matrix.Matrix $3 100 >> $5/$6-$3-$4-l.da
 echo "Matrix: stm $6, threads $3, attempt $4 medium"
 java $1 $2 -cp jvstm.jar:. jstamp.jvstm.matrix.Matrix $3 500 >> $5/$6-$3-$4-m.data
 echo "Matrix: stm $6, threads $3, attempt $4 high"
-java $1 $2 -cp jvstm.jar:. jstamp.jvstm.matrix.Matrix $3 1250 >> $5/$6-$3-$4-h.data
+java $1 $2 -cp jvstm.jar:. jstamp.jvstm.matrix.Matrix $3 1150 >> $5/$6-$3-$4-h.data
 rm jstamp/jvstm/matrix/*.class
 rm jstamp/jvstm/*.class
